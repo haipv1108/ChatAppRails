5a64810a353e209476c24075dd796fa30f8d4bb0
HaiLeader <pham.haisv@gmail.com>
2017-06-06 17:22:33 +0700

    Create server and send message

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,5 +1,4 @@
 class ChatRoomsController < ApplicationController
-
   def index
     @chat_rooms = ChatRoom.all
   end
@@ -11,15 +10,16 @@ class ChatRoomsController < ApplicationController
   def create
     @chat_room = current_user.chat_rooms.build(chat_room_params)
     if @chat_room.save
-      flash[:success] = 'Chat room added'
+      flash[:success] = 'Chat room added!'
       redirect_to chat_rooms_path
     else
-      render :new
+      render 'new'
     end
   end
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   private
